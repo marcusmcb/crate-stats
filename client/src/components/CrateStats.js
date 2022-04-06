5f7186581a01814606829c70eefb63748edb693d
Marcus McBride <marcusmcb@gmail.com>
2022-04-06 16:37:27 -0700

    added url form input to cratestats component

--- client/src/components/CrateStats.js
+++ client/src/components/CrateStats.js
@@ -1,40 +1,55 @@
-import { useState, useEffect, Fragment } from 'react'
-import axios from 'axios'
+import { useState, useEffect, Fragment } from "react";
+import { Form, FormGroup, Input } from "reactstrap";
+import axios from "axios";
 
-import Titlebar from './Titlebar'
-import './cratestats.css'
+import Titlebar from "./Titlebar";
+import "./cratestats.css";
 
 const CrateStats = () => {
-  const [songData, setSongData] = useState({})
-  const [isBusy, setIsBusy] = useState(true)
-  const [tracks, setTracks] = useState([])
+  const [songData, setSongData] = useState({});
+  const [isBusy, setIsBusy] = useState(true);
+  const [tracks, setTracks] = useState([]);
+  const [playlistLink, setPlaylistLink] = useState({
+    url: "",
+  });
 
-  let trackList = []
+  const getReport = () => {
+    console.log("HERE")
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setPlaylistLink({
+      ...playlistLink,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  let trackList = [];
 
   useEffect(() => {
     const getStats = async () => {
-      let reportData
+      let reportData;
       await axios
-        .get('http://localhost:5000/createReport')
+        .get("http://localhost:5000/createReport")
         .then((response) => {
-          console.log(response.data)
-          reportData = response.data
+          reportData = response.data;
         })
         .catch((error) => {
-          console.log(error)
-        })
-      return reportData
-    }
+          console.log(error);
+        });
+      return reportData;
+    };
     getStats().then((data) => {
       for (let i = 0; i < data.trackLog.length; i++) {
-        trackList.push(data.trackLog[i].trackId)
+        trackList.push(data.trackLog[i].trackId);
       }
-      console.log(data)
-      setTracks(trackList)
-      setSongData(data)
-      setIsBusy(false)
-    })
-  }, [])
+      console.log(data);
+      setTracks(trackList);
+      setSongData(data);
+      setIsBusy(false);
+    });
+  }, []);
 
   return (
     <div>
@@ -43,12 +58,26 @@ const CrateStats = () => {
         <p>Loading...</p>
       ) : (
         <Fragment>
+          <Form>
+            <FormGroup onSubmit={getReport}>
+              {/* <Label for='examplePassword'>Password</Label> */}
+              <Input
+                type="text"
+                name="url"
+                value={playlistLink.url}
+                onChange={handleChange}
+                placeholder="your Serato playlist URL"
+                bsSize="sm"
+              />
+            </FormGroup>
+          </Form>
+          <button type="submit">Submit</button>
+          <hr />
           <div>Your Report:</div>
         </Fragment>
-                 
       )}
     </div>
-  )
-}
+  );
+};
 
-export default CrateStats
+export default CrateStats;
