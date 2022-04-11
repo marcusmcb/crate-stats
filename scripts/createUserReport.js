3b52313abfa87741edbd6517c73684e8a0ce21a5
Marcus McBride <marcusmcb@gmail.com>
2022-04-11 11:40:29 -0700

    added base stats to createUserReport

--- scripts/createUserReport.js
+++ scripts/createUserReport.js
@@ -1,36 +1,68 @@
 const createUserReport = (data) => {
-
+  // console.log(data)
   // csv import notes:
   //
   // data[0] contains all of the playlist title/artist data
   // remainder of the array is all track info
 
+  // master array of tracks played
+  const masterTrackLog = data.slice(1);
+
   // playlist title
-  const playlistTitle = data[0].name
+  // check if title is just date or has an actual title
+  const playlistTitle = data[0].name;
 
   // serato display name
-  // check if ''
-  const playlistArtist = data[0].artist
+  // check if display name is ''
+  const playlistArtist = data[0].artist;
 
   // playlist start time
-  const playlistStartTime = data[0][ 'start time' ]
+  const playlistStartTime = data[0]["start time"];
 
   // playlist end time
-  const playlistEndTime = data[0][ 'end time' ]
+  const playlistEndTime = data[0]["end time"];
 
   // playlist length
-  const playlistLength = data[0].playtime
+  const playlistLength = data[0].playtime;
+  console.log('Playlist Length: ', playlistLength)
 
   // array of artists
-  let artistArray = []
+  let artistArray = [];
+  masterTrackLog.forEach((track) => {
+    artistArray.push(track.artist);
+  });
 
   // array of track lengths
-  const trackLengths = []
+  const trackLengths = [];
+  masterTrackLog.forEach((track) => {
+    trackLengths.push(new Date("01/01/2020 " + track.playtime));
+  });
 
-  // array of tracks played
-  const trackLog = []
+  // longest track
+  let longestTrack = trackLengths.reduce((a, b) => (a > b ? a : b));
+  const longestTrackIndex = trackLengths.indexOf(longestTrack);
+  longestTrack = masterTrackLog[longestTrackIndex];
+  console.log("Longest Track: ", longestTrack.playtime);
 
-  
-}
+  // shortest track
+  let shortestTrack = trackLengths.reduce((a, b) => (a < b ? a : b));
+  const shortestTrackIndex = trackLengths.indexOf(shortestTrack);
+  shortestTrack = masterTrackLog[shortestTrackIndex];
+  console.log("Shortest Track: ", shortestTrack.playtime);
 
-module.exports = createUserReport
+  // doubles array
+  // add logic to determine consecutive doubles
+  const doublesPlayed = []
+
+  // duplicates array
+  // add logic to determine non-conseuctive duplicates
+  const duplicatesPlayed = []
+
+  // "favored deck"
+  // determine which deck got more play time during set
+  const favoredDeckStats = []
+
+  console.log("----------------------------------");
+};
+
+module.exports = createUserReport;
