9d902bec288c52f894ea76b4fe3696bd3fdfe659
Marcus McBride <marcusmcb@gmail.com>
2022-04-18 21:22:36 -0700

    added second spans, updated styling to secondary panel

---
+++ client/src/components/spantext/secondText.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+const SecondText = () => {
+  return (
+    <span>Second</span>
+  )
+}
+
+export default SecondText
---
+++ client/src/components/spantext/secondsText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SecondsText = () => {
+  return (
+    <span>Seconds</span>
+  )
+}
+
+export default SecondsText
