0378662254d0a3ae220f8acaf054fe63f26be28f
Marcus McBride <marcusmcb@gmail.com>
2022-04-26 12:22:02 -0700

    added notes, csv requires 2nd drag before data updates, an error

--- client/src/components/DragAndDrop.js
+++ client/src/components/DragAndDrop.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FileUploader } from 'react-drag-drop-files'
 import Papa from 'papaparse'
 
@@ -7,32 +7,39 @@ import axios from 'axios'
 
 const fileTypes = ['CSV']
 
-const DragAndDrop = () => {
+const DragAndDrop = () => { 
   
-  const [parsedCsvData, setParsedCsvData] = useState([])
-
-  const handleChange = (event) => {    
-    console.log(event)
+  const [parsedCsvData, setParsedCsvData] = useState([]) 
+     
+  const handleChange = async (event) => {       
+    console.log(event)        
     Papa.parse(event, {
       header: true,
       complete: results => {
-        setParsedCsvData(results.data)
-        event.reset()
+        setParsedCsvData(results.data)        
       }
     })        
     console.log(parsedCsvData)
-    axios.post('http://localhost:5000/sendFile', parsedCsvData).then((response) => {
+    await axios.post('http://localhost:5000/sendFile', parsedCsvData).then((response) => {
       console.log('* * * * * * * * * RESPONSE FROM EXPRESS: ', response)
-    })
+    })   
+    // let dropZone = document.querySelector('.uploader')
+    // dropZone.fileOrFiles = null      
   }
+
+  // on handleChange call func that has two funcs in it
+  // 1st - handles the File event from user (await csv parse & send to express)
+  // 2nd - discards the File event in the browser (useEffect?)
 
   return (
     <div className='foo'>
       <FileUploader
+        className='uploader'
         multiple={false}
         handleChange={handleChange}
         name='file'
-        types={fileTypes}
+        types={fileTypes}   
+        // fileOrFiles={null}     
       />
       {/* <p>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p> */}
       <p>file name here</p>
