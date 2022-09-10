8bb8313f43644c615cf3729f8fee88011cfc5877
Marcus McBride <marcusmcb@gmail.com>
2022-09-09 20:16:54 -0700

    added basic key data to block

--- client/src/components/data_blocks/KeyData.js
+++ client/src/components/data_blocks/KeyData.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const KeyData = (keydata) => {
   return (
-    <div>KeyData</div>
+    <div>
+      {/* ****************************************** */}
+      {/* *********** KEY DATA **************** */}
+      {/* ****************************************** */}
+      <div className='data-block-title'>Key Data</div>
+      <div className='data-block-toprow'>
+        <div className='toprow-container'>
+          <div className='data-block-primary'>
+            {/* ****************************************** */}
+            {/* *********** PLAYLIST ARTIST / TITLE DATA * */}
+            {/* ****************************************** */}
+            <div className='playlist-block-primary-header'>Most Common Key:</div>
+            <div className='playlist-block-primary-value-main'>
+              {keydata.data.most_common_key.key}
+            </div>
+          </div>
+          <div className='data-block-primary'>
+            {/* ****************************************** */}
+            {/* *********** PLAYLIST ARTIST / TITLE DATA * */}
+            {/* ****************************************** */}
+            <div className='playlist-block-primary-header'>Least Common Key:</div>
+            <div className='playlist-block-primary-value-main'>
+              {keydata.data.least_common_key.key}
+            </div>
+          </div>          
+        <div/>          
+        </div>
+      </div>
+      <div className='data-block-bottomrow'></div>
+    </div>
   )
 }
 
