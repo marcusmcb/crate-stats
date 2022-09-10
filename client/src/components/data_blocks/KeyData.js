2ba2ea61dd0a27da8aa4a495f25d9e248eac085e
Marcus McBride <marcusmcb@gmail.com>
2022-09-09 18:00:10 -0700

    reset keydata block

--- client/src/components/data_blocks/KeyData.js
+++ client/src/components/data_blocks/KeyData.js
@@ -1,52 +1,9 @@
 import React from 'react'
-import './keydata.css'
 
 const KeyData = (keydata) => {
   return (
-    <div>
-      {/* ***************************************** */}
-      {/* ************ KEY DATA ******************* */}
-      {/* ***************************************** */}
-      <div className='data-block-title'>Key Data</div>
-      <div className='keydata-block-toprow'>
-        <div className='keydata-toprow-container'>
-          <div className='keydata-block-secondary'>
-            <div className='keydata-secondary-container'>
-              {/* ****************************************** */}
-              {/* ************ MOST COMMON KEY ************* */}
-              {/* ****************************************** */}
-              <div className='keydata-secondary-container-header'>
-                Most Common Key:
-              </div>
-              <div className='keydata-secondary-container-value'>
-                {keydata.data.most_common_key.key}
-              </div>
-              <div className='keydata-secondary-container-data'>
-                (played {keydata.data.most_common_key.times_played} times)
-              </div>
-            </div>
-            <div className='keydata-secondary-container'>
-              {/* ****************************************** */}
-              {/* ************ MOST COMMON KEY ************* */}
-              {/* ****************************************** */}
-              <div className='keydata-secondary-container-header'>
-                Least Common Key:
-              </div>
-              <div className='keydata-secondary-container-value'>
-                {keydata.data.least_common_key.key}
-              </div>
-              <div className='keydata-secondary-container-data'>
-                (played {keydata.data.least_common_key.times_played} times)
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div>KeyData</div>
   )
 }
 
 export default KeyData
-
-// add logic in serato report to return and display
-// total array of keys played and values for each
