ae2b461e14b8333ebee0a44f8bfba478f08f09fd
Marcus McBride <marcusmcb@gmail.com>
2022-10-15 14:39:48 -0700

    added styling and links to pageselect

--- client/src/components/PageSelect.js
+++ client/src/components/PageSelect.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
-import { Card, Icon, Grid, Segment } from 'semantic-ui-react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { Card, Icon, Grid, Button } from 'semantic-ui-react'
 import Titlebar from './shared/Titlebar'
 
 import './pageselect.css'
@@ -8,25 +9,47 @@ const PageSelect = () => {
   return (
     <Fragment>
       <Titlebar />
-      <Grid divided='vertically'>
+      <Grid divided='vertically' centered padded='vertically'>
         <Grid.Row columns={2}>
-          <Grid.Column>
-            <Card              
+          <Grid.Column width={6}>
+            <Card
+              fluid
               header='Live Playlist Report'
-              meta='Serato Live'
+              meta='Serato Live Playlist Analysis'
               description={[
-                'Get a real time playlist analysis from Serato'
+                'Enter your Serato Live Playlist link to get real time analytics for your DJ set or stream.',
               ].join('')}
             />
+            <Button basic animated color='black'>
+              <Button.Content visible>
+                <Icon name='headphones' />
+              </Button.Content>
+              <Button.Content hidden>
+                <Link to='/'>
+                  <Icon name='arrow right' />
+                </Link>
+              </Button.Content>
+            </Button>
           </Grid.Column>
-          <Grid.Column>
-            <Card              
+          <Grid.Column width={6}>
+            <Card
+              fluid
               header='Post Playlist Analysis'
               meta='Serato CSV Export'
               description={[
-                'Export your Serato playlist as a CSV and get an analysis report'
+                'Export your playlist as a CSV file from Serato and use it to generate your crate stats.',
               ].join('')}
             />
+            <Button basic animated color='black' onClick={() => {}}>
+              <Button.Content visible>
+                <Icon name='magnify' />
+              </Button.Content>
+              <Button.Content hidden>
+                <Link to='/testreport'>
+                  <Icon name='arrow right' />
+                </Link>
+              </Button.Content>
+            </Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
