006ca7a76eeca296a941e2ea79bdaa8b752c6df1
Marcus McBride <marcusmcb@gmail.com>
2022-10-17 19:45:58 -0700

    updated ui for bpm panel

--- client/src/components/data_blocks/BPMData.js
+++ client/src/components/data_blocks/BPMData.js
@@ -1,64 +1,109 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import { Divider } from 'semantic-ui-react'
 
 const BPMData = (bpmdata) => {
   return (
-    <div>
-      {/* ****************************************** */}
-      {/* ************* BPM DATA ******************* */}
-      {/* ****************************************** */}
-      <div className='data-block-title'>BPM Data</div>
-      <div className='data-block-toprow'>
-        <div className='toprow-container'>
-          <div className='data-block-primary'>
-            {/* ****************************************** */}
-            {/* *********** AVERAGE BPM ****************** */}
-            {/* ****************************************** */}
-            <div className='data-block-primary-header'>Average BPM</div>
-            <div className='data-block-primary-value-main'>
-              {bpmdata.data.average_bpm}
-            </div>
-          </div>
-          <div className='data-block-secondary'>
-            <div className='secondary-container'>
-              {/* ****************************************** */}
-              {/* ************ BPM RANGE ******************* */}
-              {/* ****************************************** */}
-              <div className='secondary-container-header'>BPM Range</div>
-              <div className='secondary-container-value'>
-                {bpmdata.data.bpm_range.minimum} -{' '}
-                {bpmdata.data.bpm_range.maximum}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='data-block-third'>
-          <div className='tertiary-container'>
-            <div className='tertiary-item'>
-              {/* ****************************************** */}
-              {/* *********** BIGGEST BPM CHANGE *********** */}
-              {/* ****************************************** */}
-              <div className='tertiary-item-header'>
-                Biggest BPM Change:{' '}
-                <span className='text-white'>
-                  {bpmdata.data.biggest_bpm_change.track_one.bpm} -{' '}
-                  {bpmdata.data.biggest_bpm_change.track_two.bpm}
-                </span>
-              </div>
-              <div className='timer-line'>
-                "{bpmdata.data.biggest_bpm_change.track_one.name}" into "
-                {bpmdata.data.biggest_bpm_change.track_two.name}"
-              </div>
-              <div className='tertiary-item-caption'>
-                occurred at{' '}
-                <span className='tertiary-item-timestamp'>
-                  {bpmdata.data.biggest_bpm_change.occurred_at}
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+    <Fragment>
+      <div>
+        <Typography
+          sx={{ fontSize: 20 }}
+          color='#c5e1a5'
+          fontWeight={500}
+          gutterBottom
+        >
+          bpm data:
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid item md={5} sm={12}>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item sx={3} mt={1}>
+                    <Typography sx={{ fontSize: 16, fontWeight: '500' }}>
+                      average bpm:
+                    </Typography>
+                  </Grid>
+                  <Grid item sx={3}>
+                    <Typography
+                      variant='h4'
+                      component='div'
+                      fontWeight={500}
+                      sx={{ color: '#558b2f' }}
+                    >
+                      {bpmdata.data.average_bpm}
+                    </Typography>
+                  </Grid>
+                  <Grid item sx={3} mt={1}>
+                    <Typography sx={{ fontSize: 16, fontWeight: '500' }}>
+                      bpm range:
+                    </Typography>
+                  </Grid>
+                  <Grid item sx={3}>
+                    <Typography
+                      variant='h4'
+                      component='div'
+                      fontWeight={500}
+                      sx={{ color: '#558b2f' }}
+                    >
+                      {bpmdata.data.bpm_range.minimum} -{' '}
+                      {bpmdata.data.bpm_range.maximum}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Divider />
+                <Grid container spacing={2}>
+                  <Grid item sx={4}>
+                    <Typography sx={{ fontSize: 16 }}>
+                      biggest change:
+                    </Typography>
+                    <Typography variant='h5' component='div' fontWeight={500}>
+                      {bpmdata.data.biggest_bpm_change.track_one.bpm} bpm -{' '}
+                      {bpmdata.data.biggest_bpm_change.track_two.bpm} bpm
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ marginTop: 1 }}>
+                  <Grid item sx={4}>
+                    <Typography
+                      sx={{ fontSize: 16, fontWeight: '500', color: '#558b2f' }}
+                    >
+                      from:
+                    </Typography>
+                    <Typography variant='h5' component='div' fontWeight={500}>
+                      "{bpmdata.data.biggest_bpm_change.track_one.name}"
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ marginTop: 1 }}>
+                  <Grid item sx={4}>
+                    <Typography
+                      sx={{ fontSize: 16, fontWeight: '500', color: '#558b2f' }}
+                    >
+                      into:
+                    </Typography>
+                    <Typography variant='h5' component='div' fontWeight={500}>
+                      "{bpmdata.data.biggest_bpm_change.track_two.name}"
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Typography sx={{ fontSize: 14, marginTop: 1 }}>
+                  - occurred @{' '}
+                  <span style={{ color: '#1b5e20', fontWeight: '500' }}>
+                    {bpmdata.data.biggest_bpm_change.occurred_at}
+                  </span>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Box>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
