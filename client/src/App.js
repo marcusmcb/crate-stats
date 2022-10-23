cf5b42c93cd1eceb53736bb26b015f06ef3157c0
Marcus McBride <marcusmcb@gmail.com>
2022-10-23 16:44:42 -0700

    removed app css

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import './app.css'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
