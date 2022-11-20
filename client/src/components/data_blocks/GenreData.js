9da705ad08c5bf14c148d95831bfb1fc139e48bc
Marcus McBride <marcusmcb@gmail.com>
2022-11-20 08:49:13 -0800

    added modal to tag health in genre panel

--- client/src/components/data_blocks/GenreData.js
+++ client/src/components/data_blocks/GenreData.js
@@ -1,29 +1,46 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import Typography from '@mui/material/Typography'
-import CardActions from '@mui/material/CardActions'
-import Collapse from '@mui/material/Collapse'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import ExpandMore from '../helpers/CardExpander'
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandMore from "../helpers/CardExpander";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import Modal from "@mui/material/Modal";
 
 const GenreData = (genreData) => {
-  const [expanded, setExpanded] = React.useState(false)
+  const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
-    setExpanded(!expanded)
-  }
+    setExpanded(!expanded);
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <Fragment>
       <div>
         <Typography
           sx={{ fontSize: 20 }}
-          color='#c5e1a5'
+          color="#c5e1a5"
           fontWeight={500}
           gutterBottom
         >
@@ -36,10 +53,10 @@ const GenreData = (genreData) => {
                 <CardContent>
                   <Typography>unique genres played:</Typography>
                   <Typography
-                    variant='h4'
-                    component='div'
+                    variant="h4"
+                    component="div"
                     fontWeight={500}
-                    sx={{ color: '#558b2f' }}
+                    sx={{ color: "#558b2f" }}
                   >
                     {genreData.data.unique_genres_played}
                   </Typography>
@@ -52,21 +69,21 @@ const GenreData = (genreData) => {
                   <Typography>top three genre tags from this set:</Typography>
                   {genreData.data.top_three_genres.map((item, i) => (
                     <Typography
-                      component='div'
+                      component="div"
                       fontWeight={500}
-                      sx={{ color: '#558b2f' }}
+                      sx={{ color: "#558b2f" }}
                       key={i}
                       fontSize={22}
                     >
-                      {item}{' '}
+                      {item}{" "}
                     </Typography>
                   ))}
                 </CardContent>
                 <CardActions
                   sx={{
-                    height: '4vh',
-                    backgroundColor: '#616161',
-                    color: 'white',
+                    height: "4vh",
+                    backgroundColor: "#616161",
+                    color: "white",
                   }}
                 >
                   <CardContent>tag health</CardContent>
@@ -74,25 +91,25 @@ const GenreData = (genreData) => {
                     expand={expanded}
                     onClick={handleExpandClick}
                     aria-expanded={expanded}
-                    aria-label='show more'
+                    aria-label="show more"
                   >
-                    <ExpandMoreIcon sx={{ color: 'white' }} />
+                    <ExpandMoreIcon sx={{ color: "white" }} />
                   </ExpandMore>
                 </CardActions>
-                <Collapse in={expanded} timeout='auto' unmountOnExit>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
                   <Grid container spacing={1}>
                     <Grid item xs={12} md={6} sm={12} lg={6}>
-                      <Card sx={{ minWidth: 275, boxShadow: 'none' }}>
+                      <Card sx={{ minWidth: 275, boxShadow: "none" }}>
                         <CardContent>
                           {/* crate stats card */}
                           <Typography>
                             percentage of tracks played with genre tags:
                           </Typography>
                           <Typography
-                            variant='h4'
-                            component='div'
+                            variant="h4"
+                            component="div"
                             fontWeight={500}
-                            sx={{ color: '#558b2f' }}
+                            sx={{ color: "#558b2f" }}
                           >
                             {
                               genreData.data.tag_health
@@ -104,25 +121,26 @@ const GenreData = (genreData) => {
                       </Card>
                     </Grid>
                     <Grid item xs={12} md={6} sm={12} lg={6}>
-                      <Card sx={{ minWidth: 275, boxShadow: 'none' }}>
+                      <Card sx={{ minWidth: 275, boxShadow: "none" }}>
                         <CardContent>
                           {/* crate stats card */}
                           <Typography>
                             <span>
-                              ...with "other" as their main genre:{' '}
+                              ...with "other" as their main genre:{" "}
                               <HelpOutlineIcon
                                 onClick={() => {
-                                  console.log('clicked')
+                                  console.log("clicked");
+                                  handleOpen();
                                 }}
                               />
                             </span>
                           </Typography>
 
                           <Typography
-                            variant='h4'
-                            component='div'
+                            variant="h4"
+                            component="div"
                             fontWeight={500}
-                            sx={{ color: '#558b2f' }}
+                            sx={{ color: "#558b2f" }}
                           >
                             {
                               genreData.data.tag_health
@@ -130,6 +148,36 @@ const GenreData = (genreData) => {
                             }
                             %
                           </Typography>
+                          <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                          >
+                            <Box sx={style}>
+                              <Typography
+                                id="modal-modal-title"
+                                variant="h6"
+                                component="h2"
+                                style={{ color: "#558b2f" }}
+                              >
+                                What this means...
+                              </Typography>
+                              <Typography
+                                id="modal-modal-description"
+                                sx={{ mt: 2 }}
+                              >
+                                Of the tracks you played in this set that have
+                                genre tags,{" "}
+                                <span style={{ color: '#558b2f', fontWeight: '500' }}>{
+                                  genreData.data.tag_health
+                                    .percentage_with_other_as_genre
+                                }</span>
+                                % of those tracks have 'Other' as their main
+                                genre.
+                              </Typography>
+                            </Box>
+                          </Modal>
                         </CardContent>
                       </Card>
                     </Grid>
@@ -141,9 +189,9 @@ const GenreData = (genreData) => {
         </Box>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default GenreData
+export default GenreData;
 
 // check calcs on tag health - percentage w/other as genre
