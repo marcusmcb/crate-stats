a7e60374c3689655b9ccd047c05d6bf88a4f7d5d
Marcus McBride <marcusmcb@gmail.com>
2022-12-21 22:50:49 -0800

    added bpm panel to rekordbox UI

--- client/src/pages/PlaylistReports/Rekordbox/RekordboxPlaylistReport.js
+++ client/src/pages/PlaylistReports/Rekordbox/RekordboxPlaylistReport.js
@@ -8,7 +8,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Divider } from "semantic-ui-react";
-import './rekordboxplaylistreport.css'
+import "./rekordboxplaylistreport.css";
 
 const RekordboxPlaylistReport = () => {
   const [data, setData] = useState(null);
@@ -85,112 +85,237 @@ const RekordboxPlaylistReport = () => {
             </Typography>
           </div>
         ) : (
-          <div className="data-block-two">
-            <Typography
-              sx={{ fontSize: 20 }}
-              color="white"
-              fontWeight={500}
-              gutterBottom
-            >
-              track data:
-            </Typography>
-            <Box sx={{ flexGrow: 1 }}>
-              <Grid item md={5} sm={12}>
-                <Card sx={{ minWidth: 275 }}>
-                  <CardContent>
-                    <Grid container spacing={2}>
-                      <Grid item mt={1.5}>
-                        <Typography sx={{ fontSize: 16, fontWeight: "500" }}>
-                          total tracks played:
-                        </Typography>
+          <div>
+            <div className="data-block-two">
+              <Typography
+                sx={{ fontSize: 20 }}
+                color="white"
+                fontWeight={500}
+                gutterBottom
+              >
+                track data:
+              </Typography>
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid item md={5} sm={12}>
+                  <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                      <Grid container spacing={2}>
+                        <Grid item mt={1.5}>
+                          <Typography sx={{ fontSize: 16, fontWeight: "500" }}>
+                            total tracks played:
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            variant="h3"
+                            component="div"
+                            fontWeight={500}
+                            sx={{ color: "rgba(29, 79, 145, 0.8)" }}
+                          >
+                            {data.track_data.total_tracks_played}
+                          </Typography>
+                        </Grid>
+                        <Grid item mt={1.5}>
+                          <Typography sx={{ fontSize: 16, fontWeight: "500" }}>
+                            average track length:
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            variant="h3"
+                            component="div"
+                            fontWeight={500}
+                            sx={{ color: "rgba(29, 79, 145, 0.8)" }}
+                          >
+                            {data.track_data.average_track_length}
+                          </Typography>
+                        </Grid>
                       </Grid>
-                      <Grid item>
-                        <Typography
-                          variant="h3"
-                          component="div"
-                          fontWeight={500}
-                          sx={{ color: "rgba(29, 79, 145, 0.8)" }}
-                        >
-                          {data.track_data.total_tracks_played}
-                        </Typography>
+                      <Divider />
+                      <Grid container spacing={2}>
+                        <Grid item>
+                          <Typography sx={{ fontSize: 16 }}>
+                            shortest track:
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                          >
+                            {data.track_data.shortest_track_played.title}
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                            sx={{ color: "rgba(29, 79, 145, 0.8)" }}
+                          >
+                            ({data.track_data.shortest_track_played["length"]})
+                          </Typography>
+                          <Typography sx={{ fontSize: 14, marginTop: 1 }}>
+                            - played @{" "}
+                            <span
+                              style={{ color: "rgba(29, 79, 145, 0.8)", fontWeight: "500" }}
+                            >
+                              {/* {data.track_data.shortest_track.played_at} */}
+                              No Value
+                            </span>
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography sx={{ fontSize: 16 }}>
+                            longest track:
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                          >
+                            {data.track_data.longest_track_played.title}
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                            sx={{ color: "rgba(29, 79, 145, 0.8)" }}
+                          >
+                            ({data.track_data.longest_track_played["length"]})
+                          </Typography>
+                          <Typography sx={{ fontSize: 14, marginTop: 1 }}>
+                            - played @{" "}
+                            <span
+                              style={{ color: "rgba(29, 79, 145, 0.8)", fontWeight: "500" }}
+                            >
+                              {/* {data.track_data.longest_track.played_at} */}
+                              No Value
+                            </span>
+                          </Typography>
+                        </Grid>
                       </Grid>
-                      <Grid item mt={1.5}>
-                        <Typography sx={{ fontSize: 16, fontWeight: "500" }}>
-                          average track length:
-                        </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Box>
+            </div>
+            <div className="data-block-two">
+              <Typography
+                sx={{ fontSize: 20 }}
+                color="white"
+                fontWeight={500}
+                gutterBottom
+              >
+                bpm data:
+              </Typography>
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid item md={5} sm={12}>
+                  <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                      <Grid container spacing={2}>
+                        <Grid item mt={1}>
+                          <Typography sx={{ fontSize: 16, fontWeight: "500" }}>
+                            bpm range:
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            variant="h4"
+                            component="div"
+                            fontWeight={500}
+                            sx={{ color: "rgba(29, 79, 145, 0.8)" }}
+                          >
+                            {data.bpm_data.bpm_range.minBPM} -{" "}
+                            {data.bpm_data.bpm_range.maxBPM}
+                          </Typography>
+                        </Grid>
+                        <Grid item mt={1}>
+                          <Typography sx={{ fontSize: 16, fontWeight: "500" }}>
+                            average bpm:
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            variant="h4"
+                            component="div"
+                            fontWeight={500}
+                            sx={{ color: "rgba(29, 79, 145, 0.8)" }}
+                          >
+                            {data.bpm_data.average_bpm}
+                          </Typography>
+                        </Grid>
                       </Grid>
-                      <Grid item>
-                        <Typography
-                          variant="h3"
-                          component="div"
-                          fontWeight={500}
-                          sx={{ color: "rgba(29, 79, 145, 0.8)" }}
-                        >
-                          {data.track_data.average_track_length}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                    <Divider />
-                    <Grid container spacing={2}>
-                      <Grid item>
-                        <Typography sx={{ fontSize: 16 }}>
-                          shortest track:
-                        </Typography>
-                        <Typography
-                          variant="h5"
-                          component="div"
-                          fontWeight={500}
-                        >
-                          {data.track_data.shortest_track_played.title}
-                        </Typography>
-                        <Typography
-                          variant="h5"
-                          component="div"
-                          fontWeight={500}
-                          sx={{ color: "rgba(29, 79, 145, 0.8)" }}
-                        >
-                          ({data.track_data.shortest_track_played["length"]})
-                        </Typography>
-                        <Typography sx={{ fontSize: 14, marginTop: 1 }}>
-                          - played @{" "}
-                          <span style={{ color: "#1b5e20", fontWeight: "500" }}>
-                            {/* {data.track_data.shortest_track.played_at} */}
+                      <Divider />
+                      <Grid container spacing={2}>
+                        <Grid item>
+                          <Typography sx={{ fontSize: 16 }}>
+                            biggest single bpm change:
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                            color="rgba(29, 79, 145, 0.8)"
+                          >
+                            {/* {data.bpm_data.biggest_bpm_change.track_one.bpm} bpm
+                            - {data.bpm_data.biggest_bpm_change.track_two.bpm}{" "}
+                            bpm */}
                             No Value
-                          </span>
-                        </Typography>
+                          </Typography>
+                        </Grid>
                       </Grid>
-                      <Grid item>
-                        <Typography sx={{ fontSize: 16 }}>
-                          longest track:
-                        </Typography>
-                        <Typography
-                          variant="h5"
-                          component="div"
-                          fontWeight={500}
-                        >
-                          {data.track_data.longest_track_played.title}
-                        </Typography>
-                        <Typography
-                          variant="h5"
-                          component="div"
-                          fontWeight={500}
-                          sx={{ color: "rgba(29, 79, 145, 0.8)" }}
-                        >
-                          ({data.track_data.longest_track_played["length"]})
-                        </Typography>
-                        <Typography sx={{ fontSize: 14, marginTop: 1 }}>
-                          - played @{" "}
-                          <span style={{ color: "#1b5e20", fontWeight: "500" }}>
-                            {/* {data.track_data.longest_track.played_at} */}
+                      <Grid container spacing={2} sx={{ marginTop: 1 }}>
+                        <Grid item>
+                          <Typography
+                            sx={{
+                              fontSize: 16,
+                              fontWeight: "500",
+                              color: "rgba(29, 79, 145, 0.8)",
+                            }}
+                          >
+                            from:
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                          >
+                            {/* "{data.bpm_data.biggest_bpm_change.track_one.name}" */}
                             No Value
-                          </span>
-                        </Typography>
+                          </Typography>
+                        </Grid>
                       </Grid>
-                    </Grid>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Box>
+                      <Grid container spacing={2} sx={{ marginTop: 1 }}>
+                        <Grid item>
+                          <Typography
+                            sx={{
+                              fontSize: 16,
+                              fontWeight: "500",
+                              color: "rgba(29, 79, 145, 0.8)",
+                            }}
+                          >
+                            into:
+                          </Typography>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            fontWeight={500}
+                          >
+                            {/* "{data.bpm_data.biggest_bpm_change.track_two.name}" */}
+                            No Value
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Typography sx={{ fontSize: 14, marginTop: 1 }}>
+                        - occurred @{" "}
+                        <span style={{ color: "rgba(29, 79, 145, 0.8)", fontWeight: "500" }}>
+                          {/* {data.bpm_data.biggest_bpm_change.occurred_at} */}
+                          No Value
+                        </span>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Box>
+            </div>
           </div>
         )}
       </div>
