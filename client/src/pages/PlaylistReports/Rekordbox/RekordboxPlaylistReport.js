dc3f229dd580f2f11ddd3d629deea46d56321592
Marcus McBride <marcusmcb@gmail.com>
2022-12-25 20:57:52 -0800

    bitrate panel displays sub320 tracks w/color grading

--- client/src/pages/PlaylistReports/Rekordbox/RekordboxPlaylistReport.js
+++ client/src/pages/PlaylistReports/Rekordbox/RekordboxPlaylistReport.js
@@ -182,7 +182,7 @@ const RekordboxPlaylistReport = () => {
                             fontWeight={500}
                             sx={{ color: 'rgba(29, 79, 145, 0.8)' }}
                           >
-                            ({data.track_data.shortest_track_played['length']})
+                            ({data.track_data.shortest_track_played['length'].slice(1)})
                           </Typography>
                           <Typography sx={{ fontSize: 14, marginTop: 1 }}>
                             - played @{' '}
@@ -214,7 +214,7 @@ const RekordboxPlaylistReport = () => {
                             fontWeight={500}
                             sx={{ color: 'rgba(29, 79, 145, 0.8)' }}
                           >
-                            ({data.track_data.longest_track_played['length']})
+                            ({data.track_data.longest_track_played['length'].slice(1)})
                           </Typography>
                           <Typography sx={{ fontSize: 14, marginTop: 1 }}>
                             - played @{' '}
@@ -857,27 +857,50 @@ const RekordboxPlaylistReport = () => {
                   <Grid item xs={12} md={12} sm={12} lg={12}>
                     <Card sx={{ minWidth: 275 }}>
                       <CardContent>
-                        <Typography>sub 320 tracks played:</Typography>
+                        <Typography
+                          style={{ fontWeight: '400', fontSize: '16px' }}
+                        >
+                          sub 320 tracks played:
+                        </Typography>
                         <Typography
                           variant='h4'
                           component='div'
                           fontWeight={500}
                           sx={{ color: 'rgba(29, 79, 145, 0.8)' }}
                         >
-                          {data.bitrate_data.sub320_tracks.length} <span style={{ fontSize: '26px', color: 'black' }}>(out of {data.master_track_log.length} tracks)</span>
+                          {data.bitrate_data.sub320_tracks.length}{' '}
+                          <span style={{ fontSize: '20px', color: 'black' }}>
+                            (out of {data.master_track_log.length} tracks)
+                          </span>
                         </Typography>
                         <Divider />
-                        <Typography style={{ marginBottom: '15px' }}>track title / bitrate:</Typography>
+                        <Typography
+                          style={{ marginBottom: '15px', fontSize: '16px' }}
+                        >
+                          track title & bitrate:
+                        </Typography>
                         {data.bitrate_data.sub320_tracks.map((item, key) => {
                           return (
                             <Typography
-                              variant='h5'
                               key={key}
                               component='div'
                               fontWeight={500}
-                              sx={{ color: 'rgba(29, 79, 145, 0.8)' }}
+                              sx={{
+                                color:
+                                  item.bitrate === '256 kbps'
+                                    ? 'rgba(255, 104, 39, 0.8)'
+                                    : item.bitrate === '192 kbps'
+                                    ? 'rgba(255, 39, 77, 0.8)'
+                                    : item.bitrate === '224 kbps'
+                                    ? 'rgba(255, 35, 129, 0.8)'
+                                    : 'black',
+                              }}
+                              style={{ fontSize: '20px' }}
                             >
-                              {item.title} - {item.bitrate}
+                              <span style={{ color: 'black' }}>
+                                {item.title} -
+                              </span>{' '}
+                              {item.bitrate}
                             </Typography>
                           )
                         })}
