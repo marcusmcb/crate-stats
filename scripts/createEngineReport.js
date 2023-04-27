5a43ef25e75abc0923894907c1687a6f9288ca2a
Marcus McBride <marcusmcb@gmail.com>
2023-04-26 17:12:24 -0700

    added comma replacement method for album field, quote removal for all others, working

--- scripts/createEngineReport.js
+++ scripts/createEngineReport.js
@@ -13,12 +13,21 @@ const removeQuotes = (str) => {
 // causing issue where text after comma appears
 // in the next/wrong field
 
-const convertTime = (seconds) => {
-  var minutes = Math.floor(seconds / 60); // Get the number of minutes
-  var remainingSeconds = seconds % 60; // Get the remaining seconds
+const convertSecondsToMMSS = (seconds) => {
+  var minutes = Math.floor(seconds / 60) // Get the number of minutes
+  var remainingSeconds = seconds % 60 // Get the remaining seconds
+
+  // Format the minutes and seconds with leading zeros if necessary
+  var formattedMinutes = (minutes < 10 ? '0' : '') + minutes
+  var formattedSeconds = (remainingSeconds < 10 ? '0' : '') + remainingSeconds
+
   // Format the output string
-  var output = minutes + ' minute(s), ' + remainingSeconds + ' second(s)';
-  return output;
+  var output = formattedMinutes + ':' + formattedSeconds
+  return output
+}
+
+function replaceCommasWithWhiteSpace(str) {
+  return str.replace(/,/g, ' ')
 }
 
 const createEngineReport = () => {
@@ -28,25 +37,25 @@ const createEngineReport = () => {
   )
 
   let engineDataArray = engineData.split('\n')
-  let engineDataObjects = []  
+  let engineDataObjects = []
 
   engineDataArray.forEach((track, index) => {
     if (index === 0) return // Skip the header row
-    let trackArray = track.split(',')    
+    let trackArray = track.split(',')
     if (Object.entries(trackArray).length === 1) return
-    // console.log(convertTime(removeQuotes(trackArray[4])))
+    // console.log(convertSecondsToMMSS(removeQuotes(trackArray[4])))
     let trackObject = {
       track_number: removeQuotes(trackArray[0]),
       title: removeQuotes(trackArray[1]),
       artist: removeQuotes(trackArray[2]),
-      album: removeQuotes(trackArray[3]),
-      length: removeQuotes(trackArray[4]),
+      album: replaceCommasWithWhiteSpace(removeQuotes(trackArray[3])),
+      length: convertSecondsToMMSS(removeQuotes(trackArray[4])),
       bpm: removeQuotes(trackArray[5]),
       genre: removeQuotes(trackArray[6]),
       label: removeQuotes(trackArray[7]),
       composer: removeQuotes(trackArray[8]),
       remixer: removeQuotes(trackArray[9]),
-      year: removeQuotes(trackArray[10]),      
+      year: removeQuotes(trackArray[10]),
     }
     engineDataObjects.push(trackObject)
   })
